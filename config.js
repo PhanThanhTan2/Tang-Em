b4b41006a5ae8302ae8a70ac3efe35d5190a568a
PhanThanhTan2 <85785129+PhanThanhTan2@users.noreply.github.com>
2021-06-12 16:05:12 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Bae à!',
+    introDesc: ` Ngày anh gặp em thì 
+    anh mới phát hiện ra thiên thần
+    là có thật chứ không phải nó chỉ có trong truyện cổ tích 
+    mà bà hay kể,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    btnNo: 'Không, Anh trai Tân à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh ❤',
+    btnReply: 'Gửi cho anh ❤',
+    reply: 'thật ra thì em cũng yêu anh từ lâu rồi Anh Tân à :> ',
+    mess: 'Anh biết mà 🥰. Anh Cũng Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    btnAccept: 'Okiiiii lun ❤',
+    messLink: 'https://m.me/100046803243059' //link mess của các bạn. VD: https://m.me/100046803243059
 }
